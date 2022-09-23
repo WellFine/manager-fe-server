aceda490c6d8dcb961d0b8a7820905fc9d58a399
unknown <1339531047@qq.com>
2022-09-23 16:38:17 +0800

    feat: 创建申请单后台实现

--- routes/leave.js
+++ routes/leave.js
@@ -1,5 +1,6 @@
 const router = require('koa-router')()
 const Leave = require('../models/leaveSchema')
+const Dept = require('../models/deptSchema')
 const util = require('../utils/util')
 
 router.prefix('/leave')
@@ -29,6 +30,61 @@ router.get('/list', async ctx => {
     })
   } catch (err) {
     ctx.body = util.fail(`查询申请列表失败：${err.stack}`)
+  }
+})
+
+router.post('/operate', async ctx => {
+  const { _id, action, ...params } = ctx.request.body
+  const { data } = util.decoded(ctx.request.headers.authorization)
+
+  try {
+    if (action === 'add') {
+      // 生成申请单号
+      let orderNo = 'XJ'
+      orderNo += util.formateDate(new Date(), 'yyyy-MM-dd')
+      const total = await Leave.countDocuments()
+      params.orderNo = orderNo + total  // 公司内部使用，所以编号可以不用那么严谨
+
+      // 获取用户当前部门 ID
+      const id = data.deptId.pop()
+      // 查找当前部门信息，内含负责人信息
+      const dept = await Dept.findById(id)
+      // 获取人事部门和财务部门负责人信息
+      const userList = await Dept.find({ deptName: { $in: ['人事部门', '财务部门'] } })
+
+      // 当前审批负责人
+      const curAudtiUserName = dept.userName
+      // 组装审批用户与审批流程，当前部门负责人 -> 人事部门负责人 -> 财务部门负责人
+      let auditUsers = curAudtiUserName
+      const auditFlows = [{
+        userId: dept.userId, userName: dept.userName, userEmail: dept.userEmail
+      }]
+      userList.map(item => {
+        auditUsers += `，${item.userName}`
+        auditFlows.push({
+          userId: item.userId, userName: item.userName, userEmail: item.userEmail
+        })
+      })
+
+      params.auditUsers = auditUsers
+      params.curAuditUserName = dept.userName
+      params.auditFlows = auditFlows
+      params.auditLogs = []  // 各个负责人审批后会产生一条日志插入数组中
+      params.applyUser = {
+        userId: data.userId,
+        userName: data.userName,
+        userEmail: data.userEmail
+      }
+
+      await Leave.create(params)
+      ctx.body = util.success({}, '申请成功')
+    } else {
+      // 软删除，将申请状态改为作废即可
+      await Leave.findByIdAndUpdate(_id, { applyState: 5 })
+      ctx.body = util.success({}, '作废成功')
+    }
+  } catch (error) {
+    ctx.body = util.fail(`操作失败：${error.stack}`)
   }
 })
 
--- utils/util.js
+++ utils/util.js
@@ -72,5 +72,35 @@ module.exports = {
       }
     }
     return list
+  },
+  /**
+   * 按指定规则格式化日期对象，返回格式化后的字符串
+   * @param {Date} date 日期对象
+   * @param {string} rule 格式化规则
+   * @returns 格式化后的日期字符串
+   */
+  formateDate (date, rule) {
+    let fmt = rule || 'yyyy-MM-dd hh:mm:ss'
+
+    // 年份不放进 obj 中循环来替换，是因为会受到补零操作的影响，2022 => 22
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, date.getFullYear())
+
+    const obj = {
+      'M+': date.getMonth() + 1,
+      'd+': date.getDate(),
+      'h+': date.getHours(),
+      'm+': date.getMinutes(),
+      's+': date.getSeconds()
+    }
+
+    for (const key in obj) {
+      if (new RegExp(`(${key})`).test(fmt)) {
+        const val = obj[key] + ''
+        // ('00' + val).substring(val.length) 是一种补零的方法
+        fmt = fmt.replace(RegExp.$1, ('00' + val).substring(val.length))
+      }
+    }
+
+    return fmt
   }
 }
